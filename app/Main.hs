15601aaff13eaf4fec793b7f4914f1ef0ea97870
Michael Chavinda <mschavinda@gmail.com>
2025-09-07 19:45:21 -0700

    feat: Complete num functions.

--- app/Main.hs
+++ app/Main.hs
@@ -11,21 +11,25 @@ import qualified DataFrame.PrettyPrint as D
 
 main :: IO ()
 main = do
-  let highs = [(24 :: Int), 20, 22, 23, 25, 26, 26]
-  let lows = [(14 :: Int), 13, 13, 13, 14, 15, 15]
+  let highs = [(24 :: Double), 20, 22, 23, 25, 26, 26]
+  let lows = [(14 :: Double), 13, 13, 13, 14, 15, 15]
   let df = D.fromNamedColumns [
                 ("Day", D.fromList (take (length highs) (cycle ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]))),
                 ("High Temperature (Celcius)", D.fromList highs),
                 ("Low Temperature (Celcius)", D.fromList lows)]
 
   -- Typed column references
-  let high = D.Col "High Temperature (Celcius)" :: D.Expr Int
-  let low = D.Col "Low Temperature (Celcius)" :: D.Expr Int
+  let high = D.Col "High Temperature (Celcius)" :: D.Expr Double
+  let low = D.Col "Low Temperature (Celcius)" :: D.Expr Double
 
   -- Some expressions.
   let hotDays = df
               & D.filterWhere (high `D.geq` D.Lit 25)
-              & D.derive "total" (high + low) 
-              & D.derive "year" (D.Lit (2025 :: Int)) 
+              & D.derive "high_fahrenheit" (toFahrenheit high)
+              & D.derive "low_fahrenheit" (toFahrenheit low)
+              & D.derive "average (celsius)" ((high + low) / D.Lit 2) 
   putStrLn ""
   putStrLn (D.renderMarkdownTable (Just 10) hotDays)
+
+toFahrenheit :: D.Expr Double -> D.Expr Double
+toFahrenheit t = (t * D.Lit (9 / 5)) + D.Lit 32
--- src/DataFrame/Functions.hs
+++ src/DataFrame/Functions.hs
@@ -9,7 +9,7 @@ import DataFrame.Eval
 -- | Each function must have a type class that specialies it to the
 -- | right type.
 
-class ExprEq a where
+class (Eq a) => ExprEq a where
     eq :: Expr a -> Expr a -> Expr Bool
 
 instance ExprEq Int where
@@ -25,7 +25,7 @@ instance ExprEq Bool where
     eq l r = BinaryBoolToBoolOp (==) l r
 
 
-class ExprGeq a where
+class (Ord a) => ExprGeq a where
     geq :: Expr a -> Expr a -> Expr Bool
 
 instance ExprGeq Int where
@@ -41,7 +41,7 @@ instance ExprGeq Bool where
     geq l r = BinaryBoolToBoolOp (>=) l r
 
 -- For addition
-class ExprAdd a where
+class (Num a) => ExprAdd a where
     add :: Expr a -> Expr a -> Expr a
 
 instance ExprAdd Int where
@@ -50,15 +50,98 @@ instance ExprAdd Int where
 instance ExprAdd Double where
     add l r = BinaryDoubleToDoubleOp (+) l r
 
-instance ExprAdd [Char] where
-    add l r = error "Cannot add strings"
+-- | Multiplication
+class (Num a) => ExprMult a where
+    mult :: Expr a -> Expr a -> Expr a
 
-instance ExprAdd Bool where
-    add l r = error "Cannot add bools"
+instance ExprMult Int where
+    mult l r = BinaryIntToIntOp (*) l r
 
--- | Num instance
-instance (ToColumn a, Num a, ExprAdd a) => Num (Expr a) where
+instance ExprMult Double where
+    mult l r = BinaryDoubleToDoubleOp (*) l r
+
+-- | fromInteger
+class (Num a) => ExprFromInteger a where
+    fInteger :: Integer -> Expr a
+
+instance ExprFromInteger Int where
+    fInteger = Lit . fromInteger
+
+instance ExprFromInteger Double where
+    fInteger = Lit . fromInteger
+
+-- | fromInteger
+class (Num a) => ExprNegate a where
+    neg :: Expr a -> Expr a
+
+instance ExprNegate Int where
+    neg = UnaryIntOp negate
+
+instance ExprNegate Double where
+    neg = UnaryDoubleOp negate
+
+-- | abs
+class (Num a) => ExprAbsolute a where
+    absolute :: Expr a -> Expr a
+
+instance ExprAbsolute Int where
+    absolute = UnaryIntOp abs
+
+instance ExprAbsolute Double where
+    absolute = UnaryDoubleOp abs
+
+-- | signum
+class ExprSigNum a where
+    sig :: Expr a -> Expr a
+
+instance ExprSigNum Int where
+    sig = UnaryIntOp signum
+
+instance ExprSigNum Double where
+    sig = UnaryDoubleOp signum
+
+instance (ToColumn a,
+          Num a,
+          ExprAdd a,
+          ExprFromInteger a,
+          ExprMult a,
+          ExprNegate a,
+          ExprAbsolute a,
+          ExprSigNum a) => Num (Expr a) where
     (+) = add
+    (*) = mult
+    fromInteger = fInteger
+    negate = neg
+    abs = absolute
+    signum = sig
+
+-- | fromRational
+class (Fractional a) => ExprFromRational a where
+    fromRational' :: (Fractional a) => Rational -> Expr a
+
+instance ExprFromRational Double where
+    fromRational' = Lit . fromRational
+
+-- | (/)
+class (Fractional a) => ExpDivide a where
+    divide :: Expr a -> Expr a -> Expr a
+
+instance ExpDivide Double where
+    divide = BinaryDoubleToDoubleOp (/)
+
+instance (Fractional a,
+          ToColumn a,
+          ExprFromRational a,
+          ExpDivide a,
+          Num a,
+          ExprAdd a,
+          ExprFromInteger a,
+          ExprMult a,
+          ExprNegate a,
+          ExprAbsolute a,
+          ExprSigNum a) => Fractional (Expr a) where
+    fromRational = fromRational'
+    (/) = divide
 
 filterWhere :: Expr Bool -> DataFrame -> DataFrame
 filterWhere expr df = case interpret expr df of
